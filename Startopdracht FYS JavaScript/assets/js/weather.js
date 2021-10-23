3cbf6e21965109c4fb157b8dc00cbbaf89c8ebf7
casey kruijer <caseykruijer@gmail.com>
2021-10-24 01:39:45 +0200

    sa

--- Startopdracht FYS JavaScript/assets/js/weather.js
+++ Startopdracht FYS JavaScript/assets/js/weather.js
@@ -1,37 +1,3 @@
-/**
- * Deze JavaScript methode maakt een verbinding met een externe weer server (api) om actuele informatie op te halen
- * De informatie die terugkomt is in json formaat en wordt in regel 18 in een variabele weer gestopt.
- */
-function haalWeerOp() {
-
-    // url is de link naar de externe weer server
-    const url = "http://weerlive.nl/api/json-data-10min.php?key=ebe68ec15e&locatie=Amsterdam";
-
-    //Maak verbinding met de server en haal alle informatie op (fetch)
-    fetch(url)
-        .then((response) => response.json())
-        //Doe onderstaande als alle informatie binnen is. De opgehaalde informatie wordt in een variabele data gezet.
-        .then(function (data) {
-            console.log(data);
-            // Als alle data is binnengehaald kan deze worden gebruikt in de website.
-            // Lees data uit en stop de informatie (als array) in de variabele weer
-            let weer = data.liveweer;
-            // Lees van de array positie 0 de plaats uit
-            let stad = weer[0]['plaats'];
-            // Lees van de array positie 0 de temperatuur uit
-            let temp = weer[0]['temp'];
-            // er is nog meer, bekijk de data om er achter te komen welke gegevens er nog meer zijn
-
-            document.querySelectorAll(".name")[0].innerHTML = stad;
-            document.querySelectorAll(".name")[1].innerHTML = stad;
-            document.querySelector("#temp").innerHTML = temp + " graden";
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
 // wacht dat alles van de pagina is ingeladen voordat je de onderstaande code runt.
 window.addEventListener('load',  ()  => {
 
@@ -68,3 +34,40 @@ window.addEventListener('load',  ()  => {
         }
     });
 })
+
+
+
+/**
+ * Deze JavaScript methode maakt een verbinding met een externe weer server (api) om actuele informatie op te halen
+ * De informatie die terugkomt is in json formaat en wordt in regel 18 in een variabele weer gestopt.
+ */
+function haalWeerOp() {
+
+    const url = "http://weerlive.nl/api/json-data-10min.php?key=ebe68ec15e&locatie=Amsterdam";
+
+    //Maak verbinding met de server en haal alle informatie op (fetch)
+    fetch(url)
+        .then((response) => response.json())
+        //Doe onderstaande als alle informatie binnen is. De opgehaalde informatie wordt in een variabele data gezet.
+        .then(function (data) {
+            console.log(data);
+            // Als alle data is binnengehaald kan deze worden gebruikt in de website.
+            // Lees data uit en stop de informatie (als array) in de variabele weer
+            let weer = data.liveweer;
+            // Lees van de array positie 0 de plaats uit
+            let stad = weer[0]['plaats'];
+            // Lees van de array positie 0 de temperatuur uit
+            let temp = weer[0]['temp'];
+            let verw = weer[0]['verw'];
+            // er is nog meer, bekijk de data om er achter te komen welke gegevens er nog meer zijn
+
+            document.querySelectorAll(".name")[0].innerHTML = stad;
+            document.querySelectorAll(".name")[1].innerHTML = stad;
+            document.querySelector("#temp").innerHTML = temp + " graden";
+            document.querySelector("#verw").innerHTML = verw;
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
