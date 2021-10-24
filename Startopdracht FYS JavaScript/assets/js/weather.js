84930609b60ab1dee2f9bd63fb50044446d3f34a
casey kruijer <caseykruijer@gmail.com>
2021-10-24 12:56:19 +0200

    final javascript

--- Startopdracht FYS JavaScript/assets/js/weather.js
+++ Startopdracht FYS JavaScript/assets/js/weather.js
@@ -43,31 +43,58 @@ window.addEventListener('load',  ()  => {
  */
 function haalWeerOp() {
 
-    const url = "http://weerlive.nl/api/json-data-10min.php?key=ebe68ec15e&locatie=Amsterdam";
+    if (document.getElementsByClassName("name")[0].innerHTML === "") {
+        const url = "http://weerlive.nl/api/json-data-10min.php?key=ebe68ec15e&locatie=Amsterdam";
+        fetch(url)
+            .then((response) => response.json())
+            //Doe onderstaande als alle informatie binnen is. De opgehaalde informatie wordt in een variabele data gezet.
+            .then(function (data) {
+                console.log(data);
+                // Als alle data is binnengehaald kan deze worden gebruikt in de website.
+                // Lees data uit en stop de informatie (als array) in de variabele weer
+                let weer = data.liveweer;
+                // Lees van de array positie 0 de plaats uit
+                let stad = weer[0]['plaats'];
+                // Lees van de array positie 0 de temperatuur uit
+                let temp = weer[0]['temp'];
+                let verw = weer[0]['verw'];
+                // er is nog meer, bekijk de data om er achter te komen welke gegevens er nog meer zijn
 
-    //Maak verbinding met de server en haal alle informatie op (fetch)
-    fetch(url)
-        .then((response) => response.json())
-        //Doe onderstaande als alle informatie binnen is. De opgehaalde informatie wordt in een variabele data gezet.
-        .then(function (data) {
-            console.log(data);
-            // Als alle data is binnengehaald kan deze worden gebruikt in de website.
-            // Lees data uit en stop de informatie (als array) in de variabele weer
-            let weer = data.liveweer;
-            // Lees van de array positie 0 de plaats uit
-            let stad = weer[0]['plaats'];
-            // Lees van de array positie 0 de temperatuur uit
-            let temp = weer[0]['temp'];
-            let verw = weer[0]['verw'];
-            // er is nog meer, bekijk de data om er achter te komen welke gegevens er nog meer zijn
+                document.querySelectorAll(".name")[0].innerHTML = stad;
+                document.querySelectorAll(".name")[1].innerHTML = stad;
+                document.querySelector("#temp").innerHTML = temp + " graden";
+                document.querySelector("#verw").innerHTML = verw;
 
-            document.querySelectorAll(".name")[0].innerHTML = stad;
-            document.querySelectorAll(".name")[1].innerHTML = stad;
-            document.querySelector("#temp").innerHTML = temp + " graden";
-            document.querySelector("#verw").innerHTML = verw;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }else{
+        const url = "http://weerlive.nl/api/json-data-10min.php?key=ebe68ec15e&locatie="
+            + document.getElementsByClassName("name")[0].innerHTML;
+        fetch(url)
+            .then((response) => response.json())
+            //Doe onderstaande als alle informatie binnen is. De opgehaalde informatie wordt in een variabele data gezet.
+            .then(function (data) {
+                console.log(data);
+                // Als alle data is binnengehaald kan deze worden gebruikt in de website.
+                // Lees data uit en stop de informatie (als array) in de variabele weer
+                let weer = data.liveweer;
+                // Lees van de array positie 0 de plaats uit
+                let stad = weer[0]['plaats'];
+                // Lees van de array positie 0 de temperatuur uit
+                let temp = weer[0]['temp'];
+                let verw = weer[0]['verw'];
+                // er is nog meer, bekijk de data om er achter te komen welke gegevens er nog meer zijn
 
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+                document.querySelectorAll(".name")[0].innerHTML = stad;
+                document.querySelectorAll(".name")[1].innerHTML = stad;
+                document.querySelector("#temp").innerHTML = temp + " graden";
+                document.querySelector("#verw").innerHTML = verw;
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 }
